83eae7551b2322e764443a0326a442e95e52063d
benplehn <benplehn@gmail.com>
2023-03-23 15:47:49 +0100

    Set up pytest files

---
+++ tests/test_credentials.py
@@ -0,0 +1,11 @@
+import os.path
+from google.oauth2.credentials import Credentials
+
+def test_credentials_exist():
+    # Ensure that the credentials file exists
+    assert os.path.exists('credentials.json'), 'Credentials file not found.'
+
+def test_credentials_valid():
+    # Ensure that the credentials are valid
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    assert creds.valid, 'Invalid credentials.'
