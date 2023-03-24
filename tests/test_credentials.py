41695f8f3895606a44f5452d028014a66bf85e2d
benplehn <benplehn@gmail.com>
2023-03-24 17:53:33 +0100

    again

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -5,10 +5,10 @@ SCOPES = ['https://www.googleapis.com/auth/drive']
    
 def test_credentials_exist():
     # Ensure that the credentials file exists
-   assert os.path.exists('/Users/benjaminplehn/API_ftrprf/resources/resources.json'), 'Credentials file not found.'
+   assert os.path.exists('../resources/credentials.json'), 'Credentials file not found.'
 
 
 def test_credentials_valid():
     # Ensure that the credentials are valid
-    creds = Credentials.from_authorized_user_file('/Users/benjaminplehn/API_ftrprf/resources/token.json', SCOPES)
+    creds = Credentials.from_authorized_user_file('../resources/token.json', SCOPES)
     assert creds.valid, 'Invalid credentials.'
