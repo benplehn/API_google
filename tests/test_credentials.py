159afafd6fe155f28ccf0a8ef08831efbe0f5f82
benplehn <benplehn@gmail.com>
2023-03-24 18:02:11 +0100

    changed for relative paths

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,14 +1,16 @@
-import os.path
+import os
 from google.oauth2.credentials import Credentials
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
-   
+
 def test_credentials_exist():
     # Ensure that the credentials file exists
-   assert os.path.exists('../resources/credentials.json'), 'Credentials file not found.'
-
+    credentials_file = os.path.join(os.getcwd(), 'resources/credentials.json')
+    assert os.path.exists(credentials_file), 'Credentials file not found.'
 
 def test_credentials_valid():
     # Ensure that the credentials are valid
-    creds = Credentials.from_authorized_user_file('../resources/token.json', SCOPES)
+    token_file = os.path.join(os.getcwd(), 'resources/token.json')
+    creds = Credentials.from_authorized_user_file(token_file, SCOPES)
     assert creds.valid, 'Invalid credentials.'
